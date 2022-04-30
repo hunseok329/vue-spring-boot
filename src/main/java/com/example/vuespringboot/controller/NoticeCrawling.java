e8c1dd43d8b37a5898bff7da47a1b1cde71dffbf
hunseok329 <61916401+hunseok329@users.noreply.github.com>
2022-05-01 01:38:38 +0900

    structure modification3

--- src/main/java/com/example/vuespringboot/controller/NoticeCrawling.java
+++ src/main/java/com/example/vuespringboot/controller/NoticeCrawling.java
@@ -21,28 +21,7 @@ public class NoticeCrawling {
     public String noticeCrawling() throws IOException {
 
         String notice = crawlingService.getNotice();
-        //        jsoup test code
-        String mainURL = "https://ce.daegu.ac.kr/hakgwa_home/ce/sub.php?menu=page&menu_id=30";
-        Document doc = Jsoup.connect(mainURL).get();
 
-//        System.out.println(doc.text());
-
-        Elements parsingTable = doc.getElementsByClass("board_list");
-        Elements parsingTbody = parsingTable.select("tbody");
-        Elements temp = parsingTbody.select("tr");
-        System.out.println(temp);
-
-//        for(Element item : parsingTbody){
-//            System.out.println(item);
-//        }
-
-        System.out.println("<------------------------>");
-        System.out.println("<------------------------>");
-        System.out.println("<------------------------>");
-        System.out.println("<------------------------>");
-        System.out.println("<------------------------>");
-//        System.out.println(parsingTable);
-
-        return parsingTable.html();
+        return "hello";
     }
 }
--- src/main/java/com/example/vuespringboot/service/CrawlingService.java
+++ src/main/java/com/example/vuespringboot/service/CrawlingService.java
@@ -1,10 +1,37 @@
 package com.example.vuespringboot.service;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class CrawlingService {
-    public String getNotice(){
+    public String getNotice() throws IOException {
+        //        jsoup test code
+        String mainURL = "https://ce.daegu.ac.kr/hakgwa_home/ce/sub.php?menu=page&menu_id=30";
+        Document doc = Jsoup.connect(mainURL).get();
+
+//        System.out.println(doc.text());
+
+        Elements parsingTable = doc.getElementsByClass("board_list");
+        Elements parsingTbody = parsingTable.select("tbody");
+        Elements temp = parsingTbody.select("tr");
+        System.out.println(temp);
+
+//        for(Element item : parsingTbody){
+//            System.out.println(item);
+//        }
+
+        System.out.println("<------------------------>");
+        System.out.println("<------------------------>");
+        System.out.println("<------------------------>");
+        System.out.println("<------------------------>");
+        System.out.println("<------------------------>");
+//        System.out.println(parsingTable);
+
         return "";
     }
 }
