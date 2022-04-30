8856b6b9f458409c01a0f4c3a0c1abe23fbf5509
hunseok329 <61916401+hunseok329@users.noreply.github.com>
2022-05-01 01:34:40 +0900

    structure modification2

--- src/main/java/com/example/vuespringboot/controller/NoticeCrawling.java
+++ src/main/java/com/example/vuespringboot/controller/NoticeCrawling.java
@@ -3,9 +3,11 @@ package com.example.vuespringboot.controller;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.vuespringboot.service.CrawlingService;
 
 import java.io.IOException;
 
@@ -13,8 +15,12 @@ import java.io.IOException;
 @RequestMapping("/api")
 public class NoticeCrawling {
 
+    @Autowired
+    private CrawlingService crawlingService;
     @GetMapping("/hello")
     public String noticeCrawling() throws IOException {
+
+        String notice = crawlingService.getNotice();
         //        jsoup test code
         String mainURL = "https://ce.daegu.ac.kr/hakgwa_home/ce/sub.php?menu=page&menu_id=30";
         Document doc = Jsoup.connect(mainURL).get();
---
+++ src/main/java/com/example/vuespringboot/service/CrawlingService.java
@@ -0,0 +1,10 @@
+package com.example.vuespringboot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CrawlingService {
+    public String getNotice(){
+        return "";
+    }
+}
